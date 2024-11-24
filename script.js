ab338def0ed4332654696cc33484d05f5c41911e
Fabian Mooslechner <fabian.mooslechner@gmail.com>
2024-11-24 22:09:33 +0000

    Fix stuff for github pages deploy

--- script.js
+++ script.js
@@ -15,17 +15,17 @@ function onExpandDocs(type) {
 }
 
 async function init() {
-    const labels = await fetch('/data/labels.json')
+    const labels = await fetch('./data/labels.json')
         .then(data => data.json());
-    const str = await fetch('/data/str.json')
+    const str = await fetch('./data/str.json')
         .then(data => data.json());
-    const docs = await fetch('/data/docs.json')
+    const docs = await fetch('./data/docs.json')
         .then(data => data.json());
-    const edg = await fetch('/data/egd.json')
+    const edg = await fetch('./data/egd.json')
         .then(data => data.json());
-    const sdg = await fetch('/data/sdg.json')
+    const sdg = await fetch('./data/sdg.json')
         .then(data => data.json());
-    const values = await fetch('/data/values.json')
+    const values = await fetch('./data/values.json')
         .then(data => data.json());
 
     search = new Search(labels,str, docs, edg, sdg, values);
