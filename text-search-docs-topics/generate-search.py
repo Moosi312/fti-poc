3da062f0b18c74de68752cb6a77ef62cb6bad0ac
Fabian Mooslechner <fabian.moolsechner@student.tugraz.at>
2025-01-03 08:49:27 +0100

    Add document search for indicator + topics

---
+++ text-search-docs-topics/generate-search.py
@@ -0,0 +1,82 @@
+import json
+import itertools
+import pymupdf
+from thefuzz import fuzz
+from path import Path
+
+
+TARGET_SCORE = 80
+
+
+def main():
+    docs = Path('../assets/docs/pdf')
+    text_folder = Path('./extracted-text')
+    output = Path('./docs-by-indicator.json')
+
+    strings = json.loads(Path('../assets/data/str.json').read_text())
+    labels = json.loads(Path('../assets/data/labels.json').read_text())
+    indicators = get_indicators(strings, labels)
+    topic_map = get_topic_map(strings, labels)
+
+    extract_text(docs, text_folder)
+    search_texts(text_folder, output, indicators, topic_map)
+
+
+def get_indicators(strings, labels) -> dict[str, str]:
+    indicator_keys = list(itertools.chain.from_iterable(ind['i'] for ind in strings.values()))
+    indicator_map = {i: labels[i]['short'] for i in indicator_keys}
+    return indicator_map
+
+
+def get_topic_map(strings, labels) -> dict[str, [str]]:
+    topic_map = {}
+    for key, topic in strings.items():
+        for ind in topic['i']:
+            if ind not in topic_map:
+                topic_map[ind] = []
+            topic_map[ind].append(labels[key]['short'])
+    return topic_map
+
+
+def extract_text(doc_folder: Path, text_folder: Path):
+    text_folder.rmtree_p()
+    text_folder.mkdir_p()
+
+    file_amount = len(doc_folder.files())
+
+    for i, doc_path in enumerate(doc_folder.files()):
+        print(f"\r[{i:>3}/{file_amount:>3}] Extract {doc_path.name}", end='')
+        output = text_folder / '.'.join(doc_path.name.split('.')[:-1] + ['txt'])
+        doc = pymupdf.open(doc_path)
+        with open(output, 'wb') as f:
+            for page in doc:
+                text = page.get_text().encode('utf-8')
+                f.write(text)
+
+    print(f"\r[{file_amount:>3}/{file_amount:>3}] Finished extracting text from PDFs")
+
+
+def search_texts(text_folder: Path, output: Path, indicators: dict[str: str], topic_map: dict[str: [str]]):
+    output_file = {}
+    ind_amount = len(indicators)
+    for i, (key, name) in enumerate(indicators.items()):
+        print(f"\r[{i:>3}/{ind_amount:>3}] Search for \"{name}\"", end='')
+        for text in text_folder.files():
+            if fuzz.partial_ratio(name, text.read_text()) > TARGET_SCORE:
+                if key not in output_file:
+                    output_file[key] = []
+                output_file[key].append(f"{text.stripext().name}.pdf")
+            for topic in topic_map[key]:
+                if fuzz.partial_ratio(topic, text.read_text()) > TARGET_SCORE:
+                    if key not in output_file:
+                        output_file[key] = []
+                    output_file[key].append(f"{text.stripext().name}.pdf")
+
+    print(f"\r[{ind_amount:>3}/{ind_amount:>3}] Finished searching text")
+
+    with open(output, 'w') as f:
+        json.dump(output_file, f, indent=2, ensure_ascii=False)
+
+
+if __name__ == "__main__":
+    main()
